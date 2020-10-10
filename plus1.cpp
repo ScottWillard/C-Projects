ca15ba19b16b4659c41630df189596e85e19648d
Scot Willard programming hub <Scottwillard9698@gmail.com>
2020-10-09 23:25:08 -0400

    Add files via upload

---
+++ plus1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int plus1(int x);
+
+int main() {
+
+    int x;
+
+    cout << "Enter an integer to add 1 to: " << endl;
+
+    cin >> x;
+
+    cout << plus1(x);
+
+    return 0;
+}
+
+int plus1(int x){
+
+    return x = x + 1;
+}
